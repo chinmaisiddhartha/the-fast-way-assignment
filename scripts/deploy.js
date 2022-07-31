a9b79b5837ce8bd42e47d19c83499544dc814e4f
chinmai siddhartha <chinmai@zeltatech.com>
2022-07-31 15:41:12 +0530

    Update deploy.js

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,29 +1,17 @@
-// We require the Hardhat Runtime Environment explicitly here. This is optional
-// but useful for running the script in a standalone fashion through `node <script>`.
-//
-// You can also run a script with `npx hardhat run <script>`. If you do that, Hardhat
-// will compile your contracts, add the Hardhat Runtime Environment's members to the
-// global scope, and execute the script.
-const hre = require("hardhat");
+module.exports = async function ({ getNamedAccounts, deployments }) {
+  const { deploy } = deployments
 
-async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const ONE_YEAR_IN_SECS = 365 * 24 * 60 * 60;
-  const unlockTime = currentTimestampInSeconds + ONE_YEAR_IN_SECS;
+  const { deployer } = await getNamedAccounts()
 
-  const lockedAmount = hre.ethers.utils.parseEther("1");
+  const sushi = await deployments.get("SushiToken")
 
-  const Lock = await hre.ethers.getContractFactory("Lock");
-  const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
-
-  await lock.deployed();
-
-  console.log("Lock with 1 ETH deployed to:", lock.address);
+  await deploy("SushiBar", {
+    from: deployer,
+    args: [sushi.address],
+    log: true,
+    deterministicDeployment: false
+  })
 }
 
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
-main().catch((error) => {
-  console.error(error);
-  process.exitCode = 1;
-});
+module.exports.tags = ["SushiBar"]
+module.exports.dependencies = ["UniswapV2Factory", "UniswapV2Router02", "SushiToken"]
